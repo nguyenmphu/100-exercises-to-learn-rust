ac3287ea18379ee23498bf1bf3b8f61ad7920865
nguyenmphu <minhphu.nguyen27@gmail.com>
2024-05-30 13:57:45 +0700

    feature: ex 05_10_packages

--- exercises/05_ticket_v2/10_packages/src/main.rs
+++ exercises/05_ticket_v2/10_packages/src/main.rs
@@ -1,9 +1,9 @@
 // This is a `main.rs` file, therefore `cargo` interprets this as the root of a binary target.
 
-// TODO: fix this broken import. Create a new library target in the `src` directory.
+mod hello_world;// TODO: fix this broken import. Create a new library target in the `src` directory.
 //   The library target should expose a public function named `hello_world` that takes no arguments
 //   and returns nothing.
-use packages::hello_world;
+use hello_world::hello_world;
 
 // This is the entrypoint of the binary.
 fn main() {
